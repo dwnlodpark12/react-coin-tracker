4273059c182de7175ccde8b401c3651258be1624
dwnlodpark12 <dwnlodpark12@gmail.com>
2022-05-07 23:22:06 +0900

    make fcoin tracker learn linked API With fetch

--- src/App.js
+++ src/App.js
@@ -3,25 +3,44 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [exchange, setExchange] = useState(0);
+  const onChange = (event) => setExchange(event.target.value);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=50")
+    fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
         setLoading(false);
       });
   }, []);
+  console.log(exchange);
+
   return (
     <div>
-      <h1>The Coins!</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin, index) => (
-          <li key={index}>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "Loading" : `(${coins.length})`}</h1>
+      <input
+        onChange={onChange}
+        value={exchange}
+        type="number"
+        placeholder="You can Exchange your USD to Coin! "
+      />
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {/* {coins.map((coin, index) => (
+            <option key={index}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+            </option>
+          ))} */}
+          {coins.map((coin, index) => (
+            <option key={index}>
+              {coin.name} ({coin.symbol}) : {exchange / coin.quotes.USD.price}{" "}
+              {coin.symbol}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
