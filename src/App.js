34736ba66cb21e8728fef7bb2eb3f5d2b3bbdf37
dwnlodpark12 <dwnlodpark12@gmail.com>
2022-05-09 23:54:52 +0900

    coin tracker >> trnasfer movie App

--- src/App.js
+++ src/App.js
@@ -2,47 +2,14 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [exchange, setExchange] = useState(0);
-  const onChange = (event) => setExchange(event.target.value);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    )
+      .then((res) => res.json())
+      .then((json) => console.log(json));
   }, []);
-  console.log(exchange);
-
-  return (
-    <div>
-      <h1>The Coins! {loading ? "Loading" : `(${coins.length})`}</h1>
-      <input
-        onChange={onChange}
-        value={exchange}
-        type="number"
-        placeholder="You can Exchange your USD to Coin! "
-      />
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {/* {coins.map((coin, index) => (
-            <option key={index}>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
-          ))} */}
-          {coins.map((coin, index) => (
-            <option key={index}>
-              {coin.name} ({coin.symbol}) : {exchange / coin.quotes.USD.price}{" "}
-              {coin.symbol}
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
