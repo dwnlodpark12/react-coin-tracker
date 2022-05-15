7d845fd0d050c778f8e4ee5260bf0a756d40d1e8
dwnlodpark12 <dwnlodpark12@gmail.com>
2022-05-15 23:54:07 +0900

    divide components

--- src/App.js
+++ src/App.js
@@ -1,55 +1,9 @@
 import { useEffect, useState } from "react";
 import Movie from "./components/Movie";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  // short version like '김밥'
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-    // long step1
-
-    // .then((res) => res.json())
-    // .then((json) => {
-    //   setMovies(json.data.movies);
-    //   setLoading(false);
-    // });
-
-    // step 2
-    // const json =  await response.json();
-    // setMovies(json.data.movies);
-    // setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  // short version like '김밥'
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+    // long step1
+
+    // .then((res) => res.json())
+    // .then((json) => {
+    //   setMovies(json.data.movies);
+    //   setLoading(false);
+    // });
+
+    // step 2
+    // const json =  await response.json();
+    // setMovies(json.data.movies);
+    // setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
